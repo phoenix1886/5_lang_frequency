45973d6df74287013eb41e401396235c1dbc7777
Kirill Maslov <maslovks@gmail.com>
2017-11-16 17:09:01 +0300

    variable renamed

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,7 +4,7 @@ import re
 import sys
 
 
-def load_data(filepath):
+def load_text_from_file(filepath):
     text = ''
     try:
         with open(filepath, 'r') as file:
@@ -16,42 +16,41 @@ def load_data(filepath):
     return text
 
 
-def extract_word_list_from_text(text):
+def extract_words_list_from_text(text):
     text = text.lower()
-    pattern = re.compile(r'\w+')
-    word_list = re.findall(pattern, text)
+    words_list = re.findall(r'\w+', text)
 
-    return word_list
+    return words_list
 
 
-def get_most_frequent_words(text, n_frequent_words):
-    word_list = extract_word_list_from_text(text)
-    text_counter = Counter(word_list)
-    n_frequent_words = nlargest(n_frequent_words, text_counter,
-                                key=text_counter.__getitem__)
+def get_most_frequent_words(text, number_of_words_to_show):
+    words_list = extract_words_list_from_text(text)
+    words_counter = Counter(words_list)
+    most_frequent_words = nlargest(number_of_words_to_show, words_counter,
+                                   key=words_counter.__getitem__)
 
-    return n_frequent_words
+    return most_frequent_words
 
 
 if __name__ == '__main__':
-    default_number_of_frequent_words = 10
-    default_file_path = './Герой_нашего_времени.txt'
+    default_number_of_words_to_show = 10
+    default_file_path = './Герой нашего времени.txt'
 
     file_path = sys.argv[1] if len(sys.argv) > 1 \
         else default_file_path
-    number_of_frequent_words = int(sys.argv[2]) if len(sys.argv) > 2 \
-        else default_number_of_frequent_words
+    number_of_words_to_show = int(sys.argv[2]) if len(sys.argv) > 2 \
+        else default_number_of_words_to_show
 
     try:
-        text = load_data(file_path)
+        text = load_text_from_file(file_path)
     except FileNotFoundError as err:
         print(err)
     except IOError as err:
         print(err)
     else:
         most_frequent_words_list = \
-            get_most_frequent_words(text, number_of_frequent_words)
+            get_most_frequent_words(text, number_of_words_to_show)
 
-        print("Top {} most frequent words:".format(number_of_frequent_words))
+        print("Top {} most frequent words:".format(number_of_words_to_show))
         for index, word in enumerate(most_frequent_words_list):
             print('{:2}.: {}'.format(index+1, word))
