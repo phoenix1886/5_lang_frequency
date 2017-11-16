0d04afa01f45e642baec7ca297c4bf3c2c4829ed
Kirill Maslov <maslovks@gmail.com>
2017-11-16 16:56:15 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script calculates the most frequent words used in a given text.
+It can take up to 2 arguments: 
+* path to file
+* number of words to show (optional argument, default=10)
+
+# How to use
+```
+$ python path_to_file number_of_words_to_show
+```
+
+#Examples:
+
+Example with one argument (path). By default th list of 10 most frequent word is given:
+```
+$ python lang_frequency.py '/Users/UserName/Documents/Герой_нашего_времени.txt'
+Top 10 most frequent words:
+ 1.: и
+ 2.: я
+ 3.: не
+ 4.: в
+ 5.: что
+ 6.: на
+ 7.: с
+ 8.: он
+ 9.: Я
+10.: мне
+```
+
+Example with two arguments (path, number_of_words):
+```
+$python lang_frequency.py '/Users/KirillMaslov/Documents/Герой_нашего_времени.txt' 5
+Top 5 most frequent words:
+ 1.: и
+ 2.: я
+ 3.: не
+ 4.: в
+ 5.: что
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,57 @@
+from collections import Counter
+from heapq import nlargest
+import re
+import sys
 
 
 def load_data(filepath):
-    pass
+    text = ''
+    try:
+        with open(filepath, 'r') as file:
+            text = file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError('Couldn\'t find file: {}'.format(filepath))
+    except IOError:
+        raise IOError('Couldn\'t read file: {}'.format(format(filepath)))
+    return text
 
 
-def get_most_frequent_words(text):
-    pass
+def extract_word_list_from_text(text):
+    text = text.lower()
+    pattern = re.compile(r'\w+')
+    word_list = re.findall(pattern, text)
+
+    return word_list
+
+
+def get_most_frequent_words(text, n_frequent_words):
+    word_list = extract_word_list_from_text(text)
+    text_counter = Counter(word_list)
+    n_frequent_words = nlargest(n_frequent_words, text_counter,
+                                key=text_counter.__getitem__)
+
+    return n_frequent_words
 
 
 if __name__ == '__main__':
-    pass
+    default_number_of_frequent_words = 10
+    default_file_path = './Герой_нашего_времени.txt'
+
+    file_path = sys.argv[1] if len(sys.argv) > 1 \
+        else default_file_path
+    number_of_frequent_words = int(sys.argv[2]) if len(sys.argv) > 2 \
+        else default_number_of_frequent_words
+
+    try:
+        text = load_data(file_path)
+    except FileNotFoundError as err:
+        print(err)
+    except IOError as err:
+        print(err)
+    else:
+        most_frequent_words_list = \
+            get_most_frequent_words(text, number_of_frequent_words)
+
+        print("Top {} most frequent words:".format(number_of_frequent_words))
+        for index, word in enumerate(most_frequent_words_list):
+            print('{:2}.: {}'.format(index+1, word))
